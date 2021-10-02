bef6a9c893a7ae393f03dc388e61ee3004a2ac0f
stephaniecjohansson <7steph7@gmail.com>
2021-10-02 14:51:03 -0600

    save

--- public/service_worker.js/service_worker.js
+++ public/service_worker.js/service_worker.js
@@ -7,12 +7,36 @@ const FILES_TO_CACHE = [
     "css/style.css",
     "js/index.js",
     "js/idb.js",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    // "/manifest.json",
+    // "/icons/icon-192x192.png",
+    // "/icons/icon-512x512.png"
 ]
 
 //variables to store the names of our cache
 const CACHE_NAME = 'static-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
+//install
+self.addEventListener("install", function (evt) {
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => { cache.addAll(FILES_TO_CACHE)})
+    );
+
+    self.skipWaiting();
+})
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old chache data", key)
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })    )
+        self.clients.claim();
+    })
+    
